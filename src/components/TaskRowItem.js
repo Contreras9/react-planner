45eb2dbd18c3e5314ec1fedb89b717f53dc4278f
Hamzah Contreras <h.contreras9@outlook.com>
2024-02-17 07:34:26 -0600

    Creating a New React Component

---
+++ src/components/TaskRowItem.js
@@ -0,0 +1,11 @@
+function TaskRowItem() {
+    return (
+    <tr>
+        <th scope='row'>1</th>
+        <td>Feed dog</td>
+        <td>Eric</td>
+    </tr>
+    )
+}
+
+export default TaskRowItem
