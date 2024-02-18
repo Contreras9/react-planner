d41f87a737f05af1bed99476269cf4d2ffa5cf1d
Hamzah Contreras <h.contreras9@outlook.com>
2024-02-17 22:22:07 -0600

    Handling Data using React State

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
+import React, { useState } from 'react';
 import './App.css';
 import TaskTable from './components/TaskTable';
 
 function App() {
 
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {rowNum: 1, rowDescription: "Feed puppy", rowAssigned: "User One"},
     {rowNum: 2, rowDescription: "Water plants", rowAssigned: "User Two"},
     {rowNum: 3, rowDescription: "Make dinner", rowAssigned: "User One"},
     {rowNum: 4, rowDescription: "Charge phone battery", rowAssigned: "User One"}
-  ]
+  ])
 
   const addTask = () => {
     if (tasks.length > 0) {
       const newTask = {rowNum: tasks.length + 1, rowDescription: "New Task", rowAssigned: "User Three"}
-      tasks.push(newTask)
-      console.log(tasks)
+      setTasks(tasks => [...tasks, newTask])
     }
   }
 
